3d91b515d9a75cc4120799b67fa14c4f919d53c5
sathiyavino <164841763+sathiyavino@users.noreply.github.com>
2024-04-08 10:34:29 +0530

    Update Exp2_Demultiplexer1to8.v

--- Exp2_Demultiplexer1to8.v
+++ Exp2_Demultiplexer1to8.v
@@ -1,9 +1,13 @@
-module demux_8(s,d,y);
+module demux_8(s,a,y);
 input [2:0]s;
-input d;
+input a;
 output [7:0]y;
-
-
-
-
+and(y[0],a,~s[2],~s[1],~s[0]);
+and(y[1],a,~s[2],~s[1],s[0]);
+and(y[2],a,~s[2],s[1],~s[0]);
+and(y[3],a,~s[2],s[1],s[0]);
+and(y[4],a,s[2],~s[1],~s[0]);
+and(y[5],a,s[2],~s[1],s[0]);
+and(y[6],a,s[2],s[1],~s[0]);
+and(y[7],a,s[2],s[1],s[0]);
 endmodule
