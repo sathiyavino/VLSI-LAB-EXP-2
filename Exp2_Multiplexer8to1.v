a87a8ed4c8efc37ac4a8a371cf82fc2f3b5ad738
sathiyavino <164841763+sathiyavino@users.noreply.github.com>
2024-04-08 10:36:39 +0530

    Update Exp2_Multiplexer8to1.v

--- Exp2_Multiplexer8to1.v
+++ Exp2_Multiplexer8to1.v
@@ -1,8 +1,15 @@
-module mux_8(s0,s1,s2,d,y);
-input s0,s1,s2;
-input [7:0]d;
-output y;
-
-
-
+module mux(s,c,a);
+input [2:0]s;
+input [7:0]a;
+wire [7:0]w;
+output c;
+and(w[0],a[0],~s[2],~s[1],~s[0]);
+and(w[1],a[1],~s[2],~s[1],s[0]);
+and(w[2],a[2],~s[2],s[1],~s[0]);
+and(w[3],a[3],~s[2],s[1],s[0]);
+and(w[4],a[4],s[2],~s[1],~s[0]);
+and(w[5],a[5],s[2],~s[1],s[0]);
+and(w[6],a[6],s[2],s[1],~s[0]);
+and(w[7],a[7],s[2],s[1],s[0]);
+or (c,w[0],w[1],w[2],w[3],w[4],w[5],w[6],w[7]);
 endmodule
