84dcf326d49c42d8b14518331682a15d00c96240
sathiyavino <164841763+sathiyavino@users.noreply.github.com>
2024-04-08 10:33:38 +0530

    Update Exp2_Decoder3to8.v

--- Exp2_Decoder3to8.v
+++ Exp2_Decoder3to8.v
@@ -1,7 +1,12 @@
-module decoder_8(a,b,c,y);
-input a,b,c;
+module decoder1(a,y);
+input [2:0]a;
 output[7:0]y;
-
-
-
+and(y[0],~a[2],~a[1],~a[0]);
+and(y[1],~a[2],~a[1],a[0]);
+and(y[2],~a[2],a[1],~a[0]);
+and(y[3],~a[2],a[1],a[0]);
+and(y[4],a[2],~a[1],~a[0]);
+and(y[5],a[2],~a[1],a[0]);
+and(y[6],a[2],a[1],~a[0]);
+and(y[7],a[2],a[1],a[0]);
 endmodule
