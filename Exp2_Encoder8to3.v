5a333b71718f195a757a5657f43fa79b5da3bbe2
sathiyavino <164841763+sathiyavino@users.noreply.github.com>
2024-04-08 10:35:00 +0530

    Update Exp2_Encoder8to3.v

--- Exp2_Encoder8to3.v
+++ Exp2_Encoder8to3.v
@@ -1,7 +1,7 @@
-module encoder_8(d,a,b,c);
-input [7:0]d;
-output a,b,c;
-
-
-
+module encoder(a,y);
+input [7:0]a;
+output[2:0]y;
+or(y[2],a[6],a[5],a[4],a[3]);
+or(y[1],a[6],a[5],a[2],a[1]);
+or(y[0],a[6],a[4],a[2],a[0]);
 endmodule
